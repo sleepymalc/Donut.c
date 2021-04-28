5c388c7c3d0de26c6a576400f02b5f0a1f6af66c
Hu Pingbang <bensoniscool123@gmail.com>
2021-04-28 19:18:55 +0800

    📍[Slides v1.0] Slides done

--- source_code/donut_original.c
+++ source_code/donut_original.c
@@ -3,8 +3,8 @@
              k;double sin()
          ,cos();main(){float A=
        0,B=0,i,j,z[1760];char b[
-     1760];printf("\x1b[2J");for(;;
-  ){memset(b,32,1760);memset(z,0,7040)
+    1760];printf("\x1b[2J");for(;;)
+  {memset(b,32,1760);memset(z,0,7040)
   ;for(j=0;6.28>j;j+=0.07)for(i=0;6.28
  >i;i+=0.02){float c=sin(i),d=cos(j),e=
  sin(A),f=sin(j),g=cos(A),h=d+2,D=1/(c*
@@ -17,8 +17,8 @@ in(B),t=c*h*g-f*        e;int x=40+30*D*
  ".,-~:;=!*#$@"[N>0?N:0];}}/*#****!!-*/
   printf("\x1b[H");for(k=0;1761>k;k++)
    putchar(k%80?b[k]:10);A+=0.04;B+=
-     0.02;}}/*****####*******!!=;:~
-       ~::==!!!**********!!!==::-
-         .,~~;;;========;;;:~-.
-             ..,--------,*/
+     0.02;}}/* To compile it ******
+      * gcc donut.c -lm -o donut *
+        ****** To run it *******
+            **** ./donut **/
              
